28732d42b5bac96323238c8edad281e0f6c712e9
Krakukjit Krachakjai <nutpopfnaf@gmail.com>
2026-07-11 18:11:41 +0700

    แก้เวลา Taost

--- resources/views/admin/bookings.blade.php
+++ resources/views/admin/bookings.blade.php
@@ -379,7 +379,7 @@
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
-            timer: 1000,
+            timer: 2000,
             timerProgressBar: true,
             didOpen: (toast) => {
                 toast.onmouseenter = Swal.stopTimer;
