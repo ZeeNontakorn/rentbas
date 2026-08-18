fbb16106261e64af8bb2c7918a77267673d387b0
Setthapong Homchuen <p50520.201@gmail.com>
2026-08-18 13:07:57 +0700

    ปรับ ตำแหน่งกระดิ่งกับตำแหน่งคำในหน้าให้ตรงกัน

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -169,8 +169,8 @@
                 {{-- ปุ่มแจ้งเตือน --}}
                 <div class="relative flex-shrink-0">
                     {{-- ไอคอนกระดิ่ง --}}
-                    <button id="notifBtn" class="relative focus:outline-none hover:text-gray-300 transition cursor-pointer">
-                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <button id="notifBtn" class="relative flex h-6 w-6 items-center justify-center leading-none focus:outline-none hover:text-gray-300 transition cursor-pointer">
+                        <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                   d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
                         </svg>
