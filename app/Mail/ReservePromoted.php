1b9684cc32b756594ca76c19e5eeb45ed1fb4849
Kaowweng <Boonyakorn3944@gmail.com>
2026-08-21 13:37:51 +0700

    -เพิ่มส่งอีเมลแจ้งเตือนผู้ใช้
    -ถ้าตัวสำรองได้ขึ้นตัวจริงจะมีแจ้งเตือนไปยังผู้ใช้

---
+++ app/Mail/ReservePromoted.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\GroupRound;
+use App\Models\GroupRoundSignup;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ReservePromoted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public GroupRound $round,
+        public GroupRoundSignup $signup,
+    ) {}
+
+    public function build()
+    {
+        return $this->subject('คุณได้เลื่อนขึ้นเป็นตัวจริงแล้ว! รอบ "'.$this->round->title.'"')
+            ->view('emails.reserve-promoted');
+    }
+}
